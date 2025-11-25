871593d2a215d483da14e078b674da594d77351e
Victoria-STEM <victoriasegovia001.alu@stemgranada.com>
2025-11-25 11:24:32 +0100

    feat: commit

--- polonorte/src/Game.jsx
+++ polonorte/src/Game.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { GameContext } from "./GameContext"
+import UpgradesBoard from "./UpgradesBoard"
 
 export default function Game() {
 
@@ -9,11 +10,14 @@ export default function Game() {
         <>
             <div>
                 <h3>Daño: {state.damageDealt} / {Math.round(state.waveGoal)}</h3>
-                <h3>Caramelos: {state.caramels}</h3>
+                <h3>Caramelos: {state.caramels} 🍬</h3>
                 <h3>Oleada: {state.wave}</h3>
             </div>
             <div>
                 <button className="disparar" onClick={() => dispatch({type: 'CLICK_SHOOT'})}>🏹</button>
+            </div>
+            <div>
+                <UpgradesBoard />
             </div>
         </>
     )
--- polonorte/src/UpgradesBoard.jsx
+++ polonorte/src/UpgradesBoard.jsx
@@ -1,6 +1,21 @@
+import { useContext } from "react"
+import { GameContext } from "./GameContext"
+
 export default function UpgradesBoard() {
+
+    const {state, dispatch} = useContext(GameContext)
+
     return (
         <>
+            <div>
+                <p>Multiplicador</p>
+                <button>Multiplicador </button>
+            </div>
+            <div>
+                <p>Cañón turron (15🍬)</p>
+                <p>+2 disparos automaticos</p>
+                <button onClick={() => dispatch ({type: 'BUY_DAMAGE_UPGRADE'})}>Comprar cañon</button>
+            </div>
         </>
     )
 }
